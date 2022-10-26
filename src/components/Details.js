0ae8d31f41bb6ec8571c1095e2ec0c6816c6313b
Dat Ngo <dat.ngo2994@gmail.com>
2022-10-26 21:45:51 +0700

    update new UI route /details

--- src/components/Details.js
+++ src/components/Details.js
@@ -29,123 +29,163 @@ function Details() {
 
   return (
     <div className="bg-white text-black dark:bg-gray-800 dark:text-white w-full h-full">
-      <div className="flex flex-col min-h-screen ">
+      <div className="flex flex-col">
         <Header />
-        <div className="w-screen fixed bg-transparent top-0 transition-all duration-500 z-50">
-          {/* <Header /> */}
-        </div>
-        <div className="flex-1 bg-background-1">
-          <div className="w-full">
-            <div className="relative pt-24 pb-20">
-              <div className="absolute inset-0">
-                <div className="absolute inset-0 bg-no-repeat bg-cover opacity-70 blur-xl before:content-[''] before:absolute before:inset-0 before:bg-gradient-to-b from-transparent to-background-1"></div>
+      </div>
+      <div className="bg-primary-200 pb-10 order-gray-200 bg-white px-2 py-2.5 dark:border-gray-700 dark:bg-gray-800 sm:px-4 rounded">
+        <div className="min-h-screen mt-[-1px]">
+          <div className="flex flex-col lg:flex-row">
+            <div className="pt-4 flex-1 text-text-color">
+              <div className="mb-5 text-center">
+                <h1 className="font-bold text-xl text-text-color">
+                  {mangaDetails.mangaName}
+                </h1>
+                <p className="font-semibold text-gray-500">
+                  Đã cập nhật lúc {mangaDetails.updatedAt}
+                </p>
               </div>
-              <div className="m-auto px-4 max-w-7xl lg:max-w-4xl md:max-w-2xl">
-                <div className="w-full flex items-center md:flex-col z-10">
-                  <div className="w-3/12 lg:w-4/12 md:w-8/12 md:mb-4 mr-12 md:mr-0 rounded-2xl overflow-hidden">
-                    <div className="relative pt-[160%]">
-                      <img
-                        className="absolute top-0 left-0 w-full h-full object-cover"
-                        src={mangaDetails.posterUrl}
-                        alt={mangaDetails.mangaName}
-                      />
-                    </div>
+              <div className="flex flex-col lg:flex-row">
+                <div className="flex justify-center lg:mb-0 mb-4">
+                  <div className="w-[190px] rounded-sm">
+                    <img
+                      className="border mt-4 rounded-md"
+                      src={mangaDetails.posterUrl}
+                      alt={mangaDetails.mangaName}
+                    />
                   </div>
-                  <div className="flex-1 flex flex-col justify-center text-text-0 z-[1]">
-                    <h2 className="mb-6 text-2xl font-bold line-clamp-2">
-                      {mangaDetails.mangaName}
-                    </h2>
-                    <p className="mb-4 line-clamp-5 md:line-clamp-6">
-                      {mangaDetails.description}
-                    </p>
-                    <h3 className="flex items-center">
-                      Tác giả: {mangaDetails.otherDetails.authorName}
-                    </h3>
-                    <h3 className="flex items-center">
-                      Trạng thái: {mangaDetails.otherDetails.status}
-                    </h3>
-                    <h3 className="flex items-center mb-4">
-                      Xếp hạng: {mangaDetails.otherDetails.ratingValue} (
-                      {mangaDetails.otherDetails.ratingCount} bình chọn)
-                    </h3>
-
-                    <div className="flex flex-wrap mb-4">
-                      <a
-                        className="px-[0.5rem] py-[0.25rem] mr-2 mb-2 border-2 border-solid border-white rounded-lg hover:border-primary hover:text-primary transition-all"
-                        href="/?category=comedy"
-                      >
-                        Comedy
-                      </a>
+                </div>
+                <div className="flex-1 lg:ml-10 ml-0">
+                  <div className="flex items-center justify-between">
+                    <div className="flex gap-2 flex-wrap my-3">
+                      <button>yeah</button>
+                      <button>bae</button>
+                      <button>love</button>
+                      <button>you</button>
                     </div>
-
-                    <div className="flex flex-wrap">
-                      <a
-                        className="px-4 py-2 mr-2 mb-2 rounded-lg bg-primary hover:bg-secondary transition-all"
-                        title="Chapter 1: Bản nhạc của kẻ đi dạo đêm"
-                        href="/reading/yofukashi-no-uta/chap-1/507249"
-                      >
-                        Đọc từ đầu
-                      </a>
-                      <a
-                        className="px-4 py-2 mr-2 mb-2 rounded-lg bg-primary hover:bg-secondary transition-all"
-                        title="Chapter 103"
-                        href="/reading/yofukashi-no-uta/chap-103/916989"
-                      >
-                        Đọc mới nhất
-                      </a>
-                      <button className="px-4 py-2 mr-2 mb-2 rounded-lg bg-primary hover:bg-secondary transition-all">
-                        Thêm vào thư viện
-                      </button>
-                    </div>
+                    <button className="bg-blue-500 px-3 py-1 text-sm text-text-color rounded-md font-semibold">
+                      Theo dõi
+                    </button>
+                  </div>
+                  <ul>
+                    <li className="flex text-lg font-semibold my-2">
+                      <p className="w-[100px]">Tác giả: </p>
+                      <p className="ml-4 flex-1">
+                        {mangaDetails.otherDetails.authorName}
+                      </p>
+                    </li>
+                    <li className="flex text-lg font-semibold my-2">
+                      <p className="w-[100px]">Trạng thái:</p>
+                      <p className="ml-4 flex-1">
+                        {mangaDetails.otherDetails.status}
+                      </p>
+                    </li>
+                    <li className="flex text-lg font-semibold my-2">
+                      <p className="w-[100px]">Thể loại: </p>
+                      <p className="ml-4 flex flex-wrap flex-1">
+                        <button className="mb-3 mr-3 text-text-color px-2 text-sm py-1 rounded-md bg-blue-500">
+                          Action
+                        </button>
+                        <button className="mb-3 mr-3 text-text-color px-2 text-sm py-1 rounded-md bg-blue-500">
+                          Fantasy
+                        </button>
+                        <button className="mb-3 mr-3 text-text-color px-2 text-sm py-1 rounded-md bg-blue-500">
+                          Manhua
+                        </button>
+                        <button className="mb-3 mr-3 text-text-color px-2 text-sm py-1 rounded-md bg-blue-500">
+                          Shounen
+                        </button>
+                        <button className="mb-3 mr-3 text-text-color px-2 text-sm py-1 rounded-md bg-blue-500">
+                          Supernatural
+                        </button>
+                        <button className="mb-3 mr-3 text-text-color px-2 text-sm py-1 rounded-md bg-blue-500">
+                          Truyện Màu
+                        </button>
+                      </p>
+                    </li>
+                  </ul>
+                  <div className="flex items-center mt-4">
+                    <button className="bg-green-500 px-3 py-1 text-sm text-text-color rounded-md">
+                      <a className="font-semibold">Đọc từ đầu</a>
+                    </button>
+                    <button className="bg-green-500 px-3 py-1 text-sm text-text-color rounded-md ml-5">
+                      <a className="font-semibold">Đọc mới nhất</a>
+                    </button>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="m-auto px-4 max-w-7xl lg:max-w-4xl md:max-w-2xl">
-              <h2 className="flex items-center mb-4 text-xl text-primary">
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  stroke-width="0"
-                  viewBox="0 0 512 512"
-                  className="mr-2"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M48 48a48 48 0 1 0 48 48 48 48 0 0 0-48-48zm0 160a48 48 0 1 0 48 48 48 48 0 0 0-48-48zm0 160a48 48 0 1 0 48 48 48 48 0 0 0-48-48zm448 16H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0-320H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16zm0 160H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z"></path>
-                </svg>
-                Danh sách Chapter:
-              </h2>
-              <div className="max-h-[70vh] w-full mb-4 p-4 md:p-2 rounded-lg bg-background-3 scrollbar">
-                <table className="table-auto w-full text-white">
-                  <thead>
-                    <tr>
-                      <td className="font-bold p-2 w-5/12 sm:w-7/12">
-                        <button className="flex items-center hover:text-primary transition">
-                          Chapter
-                          <svg
-                            stroke="currentColor"
-                            fill="currentColor"
-                            stroke-width="0"
-                            viewBox="0 0 320 512"
-                            className="ml-[0.25rem]"
-                            height="1em"
-                            width="1em"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"></path>
-                          </svg>
-                        </button>
-                      </td>
-                      <td className="font-bold p-2 w-4/12 text-center sm:w-5/12">
-                        Cập nhật
-                      </td>
-                      <td className="font-bold p-2 w-3/12 text-center">Xem</td>
-                    </tr>
-                  </thead>
-                  <tbody></tbody>
-                </table>
+
+              <div className="mt-4">
+                <div className="flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-6 h-6 text-blue-500 mr-1"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                    ></path>
+                  </svg>
+                  <h1 className="text-xl font-semibold text-text-color">
+                    Nội dung
+                  </h1>
+                </div>
+                <p className="font-semibold mt-4">{mangaDetails.description}</p>
+              </div>
+
+              <div className="mt-4">
+                <div className="flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    class="w-6 h-6 text-blue-500 mr-1"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <h1 className="text-xl font-semibold text-text-color">
+                    Danh sách chương
+                  </h1>
+                </div>
+              </div>
+
+              <div className="border mt-4 text-text-color rounded-md">
+                <ul className="w-full h-[350px] overflow-y-scroll">
+                  <li>
+                    <a className="flex items-center justify-between p-2">
+                      <p className="font-semibold">Chapter 2713</p>
+                      <p className="text-gray-500 text-sm">7 giờ trước</p>
+                    </a>
+                  </li>
+                  <li>
+                    <a className="flex items-center justify-between p-2">
+                      <p className="font-semibold">Chapter 2712</p>
+                      <p className="text-gray-500 text-sm">7 giờ trước</p>
+                    </a>
+                  </li>
+                  <li>
+                    <a className="flex items-center justify-between p-2">
+                      <p className="font-semibold">Chapter 2711</p>
+                      <p className="text-gray-500 text-sm">7 giờ trước</p>
+                    </a>
+                  </li>
+                  <li>
+                    <a className="flex items-center justify-between p-2">
+                      <p className="font-semibold">Chapter 2710</p>
+                      <p className="text-gray-500 text-sm">7 giờ trước</p>
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
