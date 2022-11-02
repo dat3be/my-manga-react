06c7389797fd5a95dd3cf643adfc1642b11deadc
Dat Ngo <dat.ngo2994@gmail.com>
2022-11-02 13:58:29 +0700

    update home url

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
     <header>
       <div>
         <Navbar fluid={true} rounded={true}>
-          <Navbar.Brand href="http://localhost:9000/">
+          <Navbar.Brand href="/">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="mr-3 h-6 sm:h-9"
